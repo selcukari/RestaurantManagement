4e0fa911c2b8c70442b0e21f45cbd60683c2f54c
selcuk.ari <selcukari51@gmail.com>
2025-11-17 19:40:49 +0300

    review

---
+++ shared/RestaurantManagement.Shared/Services/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Shared.Services
+{
+    public class IIdentityService
+    {
+        Guid UserId { get; }
+        string UserName { get; }
+
+        List<string> Roles { get; }
+    }
+}
---
+++ shared/RestaurantManagement.Shared/Services/IdentityService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace RestaurantManagement.Shared.Services
+{
+    internal class IdentityService(IHttpContextAccessor httpContextAccessor) : IIdentityService
+    {
+        public Guid UserId
+        {
+            get
+            {
+                if (!httpContextAccessor.HttpContext!.User.Identity!.IsAuthenticated)
+                    throw new UnauthorizedAccessException("User is not authenticated.");
+                return Guid.Parse(
+                    httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(c =>
+                        c.Type == ClaimTypes.NameIdentifier)!.Value!);
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                if (!httpContextAccessor.HttpContext!.User.Identity!.IsAuthenticated)
+                    throw new UnauthorizedAccessException("User is not authenticated.");
+
+                return httpContextAccessor.HttpContext!.User.Identity!.Name!;
+            }
+        }
+
+        public List<string> Roles
+        {
+            get
+            {
+                if (!httpContextAccessor.HttpContext!.User.Identity!.IsAuthenticated)
+                    throw new UnauthorizedAccessException("User is not authenticated.");
+
+
+                return httpContextAccessor.HttpContext!.User.Claims.Where(x => x.Type == ClaimTypes.Role)
+                    .Select(x => x.Value!)
+                    .ToList();
+            }
+        }
+    }
+}
