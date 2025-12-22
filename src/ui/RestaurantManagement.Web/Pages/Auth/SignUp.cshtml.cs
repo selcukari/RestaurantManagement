92b2df389cabab0473b74567c88535e419a567e3
selcuk.ari <selcukari51@gmail.com>
2025-12-22 23:46:37 +0300

    review

--- src/ui/RestaurantManagement.Web/Pages/Auth/SignUp.cshtml.cs
+++ src/ui/RestaurantManagement.Web/Pages/Auth/SignUp.cshtml.cs
@@ -1,15 +1,37 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RestaurantManagement.Web.Auth.SignUp;
+using RestaurantManagement.Web.Pages.Auth.SignUp;
 
 namespace RestaurantManagement.Web.Auth
 {
-    public class SignUpModel : PageModel
+    public class SignUpModel(SignUpService signUpService) : PageModel
     {
         [BindProperty] public required SignUpViewModel SignUpViewModel { get; set; } = SignUpViewModel.GetExampleModel;
 
+        // sayfa yuklenirken calýsýr
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var result = await signUpService.CreateAccount(SignUpViewModel);
+
+
+            if (result.IsFail)
+            {
+                ModelState.AddModelError(string.Empty, result.Fail.Title);
+
+                if (!string.IsNullOrEmpty(result.Fail.Detail)) ModelState.AddModelError(string.Empty, result.Fail.Detail);
+
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
         }
     }
 }
