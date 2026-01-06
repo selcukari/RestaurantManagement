a84e3c9d17102c3c60656a699250b6323bb42546
selcuk.ari <selcukari51@gmail.com>
2026-01-06 17:39:35 +0300

    addoptionext has been added

---
+++ src/services/kitchen/RestaurantManagement.Kitchen.Api/Options/MongoOption.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantManagement.Kitchen.Api.Options
+{
+    public class MongoOption
+    {
+        [Required] public string DatabaseName { get; set; } = default!;
+        [Required] public string ConnectionString { get; set; } = default!;
+    }
+}
--- src/services/kitchen/RestaurantManagement.Kitchen.Api/Program.cs
+++ src/services/kitchen/RestaurantManagement.Kitchen.Api/Program.cs
@@ -1,10 +1,12 @@
 using RestaurantManagement.Kitchen.Api;
+using RestaurantManagement.Kitchen.Api.Options;
 using RestaurantManagement.Shared.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddOptionsExt();
 builder.Services.AddCommonServiceExt(typeof(KitchenAssembly));
 
 
