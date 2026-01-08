1cd1609aa30b50dbb9d9f2ffacb89c0482fdec40
selcuk.ari <selcukari51@gmail.com>
2026-01-09 00:24:29 +0300

    review appdbcontex review

--- src/services/reservation/RestaurantManagement.Reservation.Api/Repositories/AppDbContext.cs
+++ src/services/reservation/RestaurantManagement.Reservation.Api/Repositories/AppDbContext.cs
@@ -1,8 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using RestaurantManagement.Reservation.Api.Features.Tables;
+using System.Reflection;
 
 namespace RestaurantManagement.Reservation.Api.Repositories
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        public DbSet<Table> Tables { get; set; }
+
+
+        public static AppDbContext Create(IMongoDatabase database)
+        {
+            var optionsBuilder =
+                new DbContextOptionsBuilder<AppDbContext>().UseMongoDB(database.Client,
+                    database.DatabaseNamespace.DatabaseName);
+
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
